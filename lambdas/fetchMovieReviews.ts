5bdd51bf6b24ad33dce5918369678e9e5d1a39c3
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2024-03-09 22:35:15 +0000

    Add API handler for querying movie reviews
    - Implement a new APIGatewayProxyHandlerV2 to handle movie review queries
    - Validate incoming query parameters against a JSON schema
    - Use DynamoDBDocumentClient to execute a query command on DynamoDB table
    - Return query results as JSON response with appropriate status codes
    - Implement error handling for potential exceptions during execution

---
+++ lambdas/fetchMovieReviews.ts
@@ -0,0 +1,97 @@
+import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+import { MovieReviewQueryParams } from "../shared/types";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  QueryCommand,
+  QueryCommandInput,
+} from "@aws-sdk/lib-dynamodb";
+import Ajv from "ajv";
+import schema from "../shared/types.schema.json";
+
+const ajv = new Ajv();
+const isValidQueryParams = ajv.compile(
+  schema.definitions["MovieReviewQueryParams"] || {}
+);
+
+const ddbDocClient = createDocumentClient();
+
+export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
+  try {
+    console.log("Event: ", event);
+    const queryParams = event.pathParameters;
+    if (!queryParams) {
+      return {
+        statusCode: 500,
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ message: "Missing query parameters" }),
+      };
+    }
+    if (!isValidQueryParams(queryParams)) {
+      return {
+        statusCode: 500,
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          message: `Incorrect type. Must match Query parameters schema`,
+          schema: schema.definitions["MovieReviewQueryParams"],
+        }),
+      };
+    }
+
+    const movieId = queryParams?.movieId
+      ? parseInt(queryParams.movieId)
+      : undefined;
+    let commandInput: QueryCommandInput = {
+      TableName: process.env.TABLE_NAME,
+    };
+
+    commandInput = {
+      ...commandInput,
+      KeyConditionExpression: "movieId = :m",
+      ExpressionAttributeValues: {
+        ":m": movieId,
+      },
+    };
+
+    const commandOutput = await ddbDocClient.send(
+      new QueryCommand(commandInput)
+    );
+
+    return {
+      statusCode: 200,
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        data: commandOutput.Items,
+      }),
+    };
+  } catch (error: any) {
+    console.log(JSON.stringify(error));
+    return {
+      statusCode: 500,
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ error }),
+    };
+  }
+};
+
+function createDocumentClient() {
+  const ddbClient = new DynamoDBClient({ region: process.env.REGION });
+  const marshallOptions = {
+    convertEmptyValues: true,
+    removeUndefinedValues: true,
+    convertClassInstanceToMap: true,
+  };
+  const unmarshallOptions = {
+    wrapNumbers: false,
+  };
+  const translateConfig = { marshallOptions, unmarshallOptions };
+  return DynamoDBDocumentClient.from(ddbClient, translateConfig);
+}
